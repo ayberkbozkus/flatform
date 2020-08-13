5a9691f2646a5e8d6911e8c06cac23224c5fe9b9
ayberkbozkus <ayberkbozkus@gmail.com>
2020-08-13 19:23:56 +0300

    update last code

--- lib/screens/NavBar.dart
+++ lib/screens/NavBar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
-import './pieChart.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatefulWidget {
