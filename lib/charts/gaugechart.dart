99180639cd56c83706bb4127ac6f54cb4a8c9f17
ayberkbozkus <ayberkbozkus@gmail.com>
2020-08-17 10:28:44 +0300

    no line chart, expect okay

--- lib/charts/gaugechart.dart
+++ lib/charts/gaugechart.dart
@@ -20,15 +20,6 @@ class GaugeChart extends StatelessWidget {
     );
   }
 
-  gaugeChartDisplay = new charts.PieChart(seriesList,
-        animate: animate,
-        // Configure the width of the pie slices to 30px. The remaining space in
-        // the chart will be left as a hole in the center. Adjust the start
-        // angle and the arc length of the pie so it resembles a gauge.
-        defaultRenderer: new charts.ArcRendererConfig(
-            arcWidth: 30, startAngle: 4 / 5 * pi, arcLength: 7 / 5 * pi));
-  
-
   @override
   Widget build(BuildContext context) {
     return new charts.PieChart(seriesList,
--- lib/screens/NavBar.dart
+++ lib/screens/NavBar.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   static var barchartdisplay;
+  static var wbarchartdisplay;
   static var piechartdisplay;
   void initState() {
     setState(() {
@@ -27,6 +28,22 @@ class _HomePageState extends State<HomePage> {
         addCharts("11", 12),
         addCharts("12", 35),
       ];
+      var tdata = [
+        addCharts("1", 13),
+        addCharts("2", 23),
+        addCharts("3", 27),
+        addCharts("4", 9),
+        addCharts("5", 17),
+      ];
+      var wdata = [
+        addCharts("P", 13),
+        addCharts("S", 23),
+        addCharts("C", 27),
+        addCharts("P", 9),
+        addCharts("C", 17),
+        addCharts("C", 31),
+        addCharts("P", 18),
+      ];
       var series = [
         charts.Series(
           domainFn: (addCharts addCharts, _) => addCharts.label,
@@ -35,12 +52,32 @@ class _HomePageState extends State<HomePage> {
           data: data,
         ),
       ];
+      var wseries = [
+        charts.Series(
+          domainFn: (addCharts addCharts, _) => addCharts.label,
+          measureFn: (addCharts addCharts, _) => addCharts.values,
+          id: 'waddcharts',
+          data: wdata,
+        ),
+      ];
+      var tseries = [
+        charts.Series(
+          domainFn: (addCharts addCharts, _) => addCharts.label,
+          measureFn: (addCharts addCharts, _) => addCharts.values,
+          id: 'taddcharts',
+          data: tdata,
+        ),
+      ];
       barchartdisplay = charts.BarChart(
         series,
         animationDuration: Duration(microseconds: 2000),
       );
+      wbarchartdisplay = charts.BarChart(
+        wseries,
+        animationDuration: Duration(microseconds: 2000),
+      );
       piechartdisplay = charts.PieChart(
-        series,
+        tseries,
         animationDuration: Duration(microseconds: 2000),
       );
     });
@@ -92,9 +129,61 @@ class _HomePageState extends State<HomePage> {
     ],
   );
 
+  Widget sfRadialGaugedisplayP = SfRadialGauge(
+    axes: <RadialAxis>[
+      RadialAxis(
+        minimum: 0,
+        maximum: 300,
+        interval: 50,
+        ranges: <GaugeRange>[
+          GaugeRange(
+            startValue: 0,
+            endValue: 50,
+            color: Colors.green,
+          ),
+          GaugeRange(
+            startValue: 50,
+            endValue: 100,
+            color: Colors.yellow,
+          ),
+          GaugeRange(
+            startValue: 100,
+            endValue: 150,
+            color: Colors.orange,
+          ),
+          GaugeRange(
+            startValue: 150,
+            endValue: 300,
+            color: Colors.red,
+          ),
+        ],
+        pointers: <GaugePointer>[
+          NeedlePointer(
+            value: 42,
+            enableAnimation: true,
+          )
+        ],
+        annotations: <GaugeAnnotation>[
+          GaugeAnnotation(
+            widget: Text(
+              "42",
+              style: TextStyle(
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            positionFactor: 0.5,
+            angle: 90,
+          )
+        ],
+      ),
+    ],
+  );
+
   var data = [0.0, 1.0, 1.5, 2.0];
   int _currentIndex = 0;
-  Material myItems(IconData icon, String heading, int color) {
+
+  Material mybarItems(IconData icon, String heading, int color) {
     return Material(
       color: Colors.white,
       elevation: 5.0,
@@ -126,15 +215,262 @@ class _HomePageState extends State<HomePage> {
                     children: <Widget>[
                       Center(
                         child: new Container(
-                            height: 200,
+                            height: 150,
                             width:
                                 200, //piechartdisplay    barchartdisplay     sfRadialGaugedisplay
-                            child: sfRadialGaugedisplay),
+                            child: barchartdisplay),
                       ),
-                      Center(
-                        child: Text("denemedenemedeneme"),
+                      Column(
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              "42.332 adet",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              "Toplam",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black38,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                          ),
+                          Center(
+                              child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      "%69",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.orange,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    Text(
+                                      "Üretilen",
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      "3.22",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.orange,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    Text(
+                                      "Gereken Zaman",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )),
+                        ],
                       )
                     ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material mywbarItems(IconData icon, String heading, int color) {
+    return Material(
+      color: Colors.white,
+      elevation: 5.0,
+      shadowColor: Colors.blue[200],
+      borderRadius: BorderRadius.circular(10.0),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // text
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      heading,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                  //Icon
+
+                  Center(
+                    child: new Container(
+                        height: 150,
+                        width: 150,
+                        //piechartdisplay    barchartdisplay     sfRadialGaugedisplay      wbarchartdisplay
+                        child: wbarchartdisplay),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material myradialItems(IconData icon, String heading, int color) {
+    return Material(
+      color: Colors.white,
+      elevation: 5.0,
+      shadowColor: Colors.blue[200],
+      borderRadius: BorderRadius.circular(10.0),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // text
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      heading,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                  //Icon
+
+                  Center(
+                    child: new Container(
+                        height: 150,
+                        width:
+                            150, //piechartdisplay    barchartdisplay     sfRadialGaugedisplay    sfRadialGaugedisplayP
+                        child: sfRadialGaugedisplay),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material myradialItemsP(IconData icon, String heading, int color) {
+    return Material(
+      color: Colors.white,
+      elevation: 5.0,
+      shadowColor: Colors.blue[200],
+      borderRadius: BorderRadius.circular(10.0),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // text
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      heading,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                  //Icon
+
+                  Center(
+                    child: new Container(
+                        height: 150,
+                        width:
+                            150, //piechartdisplay    barchartdisplay     sfRadialGaugedisplay    sfRadialGaugedisplayP
+                        child: sfRadialGaugedisplayP),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Material mypieItems(IconData icon, String heading, int color) {
+    return Material(
+      color: Colors.white,
+      elevation: 5.0,
+      shadowColor: Colors.blue[200],
+      borderRadius: BorderRadius.circular(10.0),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // text
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      heading,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ),
+                  //Icon
+
+                  Center(
+                    child: new Container(
+                        height: 150,
+                        width:
+                            150, //piechartdisplay    barchartdisplay     sfRadialGaugedisplay
+                        child: piechartdisplay),
                   ),
                 ],
               ),
@@ -171,18 +507,20 @@ class _HomePageState extends State<HomePage> {
           mainAxisSpacing: 10.0,
           padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
           children: <Widget>[
-            myItems(Icons.graphic_eq, "Çalışan Makine Sayısı", 0xff7297ff),
-            myItems(Icons.bookmark, "Toplam Üretim Hatalı/Hatasız", 0xff7297ff),
-            myItems(Icons.notifications, "Toplam Üretim", 0xff7297ff),
-            myItems(Icons.attach_money, "Hatalı Parça", 0xff7297ff),
-            myItems(Icons.settings, "Tesis Verimliliği", 0xff7297ff),
+            myradialItems(
+                Icons.graphic_eq, "Çalışan Makine Sayısı", 0xff7297ff),
+            mywbarItems(
+                Icons.bookmark, "Toplam Üretim\nHatalı/Hatasız", 0xff7297ff),
+            mybarItems(Icons.notifications, "Toplam Üretim", 0xff7297ff),
+            myradialItemsP(Icons.attach_money, "Hatalı Parça", 0xff7297ff),
+            mypieItems(Icons.settings, "Tesis Verimliliği", 0xff7297ff),
           ],
           staggeredTiles: [
-            StaggeredTile.extent(2, 250.0),
-            StaggeredTile.extent(2, 250.0),
-            StaggeredTile.extent(2, 250.0),
-            StaggeredTile.extent(2, 250.0),
-            StaggeredTile.extent(2, 250.0),
+            StaggeredTile.extent(1, 220.0),
+            StaggeredTile.extent(1, 220.0),
+            StaggeredTile.extent(2, 200.0),
+            StaggeredTile.extent(1, 200.0),
+            StaggeredTile.extent(1, 200.0),
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -221,4 +559,11 @@ class addCharts {
   final String label;
   final int values;
   addCharts(this.label, this.values);
+}
+
+class Task {
+  String task;
+  String taskvalue;
+  Color colorval;
+  Task(this.task, this.taskvalue, this.colorval);
 }
